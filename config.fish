705c0014f7380c69a73ecc8aea9903833c5add0c
Maciej Sroka <mck159@gmail.com>
2018-09-09 17:50:00 +0200

    Fishconfig (pbcopy)

---
+++ config.fish
@@ -0,0 +1,1 @@
+alias pbcopy="head -c -1 | xclip -selection clipboard"
--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ echo `pwd`
 
 ln -s "`pwd`/gitconfig" ~/.gitconfig
 ln -s "`pwd`/vimrc" ~/.vimrc
+ln -s "`pwd`/config.fish" ~/.config/fish/config.fish
